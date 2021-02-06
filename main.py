00ae3e735ff41cfe47c2f55b51b7109c3d3b8813
THorvath1 <72626710+THorvath1@users.noreply.github.com>
2021-02-05 17:55:48 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+my_url = 'https://www.newegg.com/Laptops-Notebooks/Category/ID-223?Tid=17489'
+
+# loading connection/grabbing page
+xClient = requests.get(my_url)
+
+
+# html parsing
+page_soup = BeautifulSoup(p_html, "html.parser")
+
+#grabs each laptop
+containers = page_soup.findAll("div", {"class":"item-container"})
+
+filename = "laptops.csv"
+f = open(filename, "w")
+headers = "brand", "product_name", "shipping\n"
+f.write(headers)
+
+# for loop that extracts titles of laptops
+for container in containers:
+	brand = container.div.div.a.img["title"]
+
+	title_container = container.findAll("a", {"class":"item-title"})
+	product_name = title_container[0].text
+
+	shipping_container = container.findAll("li", {"class" : "price-ship"})
+	shipping = shipping_container[0].text.strip()
+
+	print("brand: " + brand)
+	print("product_name: " + product_name)
+	print("shipping: " + shipping)
+
+	f.write(brand + ", " + product_name.replace(",", "|") + ", " + shipping + "\n")
+
+	f.close()
+
+ 
